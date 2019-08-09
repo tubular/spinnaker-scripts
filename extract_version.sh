634189473558a7d2675831daef051cde48d30dc4
Yevgeniy Korin <ykorin@tubularlabs.com>
2019-08-09 18:59:30 +0300

    updated script

--- extract_version.sh
+++ extract_version.sh
@@ -2,7 +2,8 @@
 
 git clone git@github.com:tubular/tbcode.git
 git clone git@github.com:tubular/spinnaker-scripts.git
-grep "current_version = " ./tbcode/svc_dummyhack/.bumpversion.cfg | sed 's/current_version = //' > ./spinnaker-scripts/version.txt
+VERSION=`grep "current_version = " ./tbcode/svc_dummyhack/.bumpversion.cfg | sed 's/current_version = //'`
+echo "{image: {tag: $VERSION}}"~ > ./spinnaker-scripts/version.txt
 cd ./spinnaker-scripts
 git add version.txt
 git commit -m "update version"
